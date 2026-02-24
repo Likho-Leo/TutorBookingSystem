effeedd4fcfae61d9e899ec88e22ebd796c598d7
Likho-Leo <likholeohla15@gmail.com>
2026-02-24 18:03:28 +0200

    Created View Models(ChangePassword, Login, Register and verifyEmail)

---
+++ TutorBookingSystem/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TutorBookingSystem.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }        
+    }
+}
