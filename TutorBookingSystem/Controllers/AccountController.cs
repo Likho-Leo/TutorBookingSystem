9a08c5dca5af030494a04997fe7e01dcf5de7ee2
Likho-Leo <likholeohla15@gmail.com>
2026-02-26 17:24:25 +0200

    created login controller

--- TutorBookingSystem/Controllers/AccountController.cs
+++ TutorBookingSystem/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TutorBookingSystem.Models;
+using TutorBookingSystem.ViewModels;
 
 namespace TutorBookingSystem.Controllers
 {
@@ -21,6 +22,26 @@ namespace TutorBookingSystem.Controllers
         public IActionResult Login()
         { 
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid Login Attempt.");
+            return View(model);
         }
     }
 }
