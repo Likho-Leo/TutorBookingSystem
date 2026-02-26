7ba0f66c3b3b8f2b1e5750dac43a27e0aeb5a5ad
Likho-Leo <likholeohla15@gmail.com>
2026-02-26 16:16:50 +0200

    Created Login view

--- TutorBookingSystem/Controllers/AccountController.cs
+++ TutorBookingSystem/Controllers/AccountController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TutorBookingSystem.Models;
 
 namespace TutorBookingSystem.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly SignInManager<User> signInManager;
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<IdentityRole<int>> roleManager;
+
+        public AccountController(SignInManager<User> signInManager, UserManager<User> userManager, RoleManager<IdentityRole<int>> roleManager)
         {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        { 
             return View();
         }
     }
